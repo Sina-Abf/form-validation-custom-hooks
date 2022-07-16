1bf8dfcb95444942ef3d3824c4a016cd47e9ca08
Sina-Abf <abf.sina@gmail.com>
2022-07-16 22:49:58 +0430

    seprate formInput component to its own

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,56 +1,7 @@
-import React from 'react';
+import FormInput from './FormInput';
 
 const Form = () => {
-  return (
-    <form className=" bg-cyan-600 rounded-xl p-20 flex flex-col gap-y-4">
-      <section className="flex gap-x-4">
-        <div className="">
-          <label
-            className="text-white text-lg font-semibold block mb-1"
-            htmlFor="firstname"
-          >
-            First Name
-          </label>
-          <input
-            type="text"
-            id="firstname"
-            className="px-2 py-0.5 outline-none rounded-md "
-          />
-        </div>
-        <div className="">
-          <label
-            className="text-white text-lg font-semibold block mb-1"
-            htmlFor="lastname"
-          >
-            Last Name
-          </label>
-          <input
-            type="text"
-            id="lastname"
-            className="px-2 py-0.5 outline-none rounded-md "
-          />
-        </div>
-      </section>
-      <section className="flex gap-x-4 mb-6">
-        <div className="">
-          <label
-            className="text-white text-lg font-semibold block mb-1"
-            htmlFor="email"
-          >
-            E-mail
-          </label>
-          <input
-            type="text"
-            id="email"
-            className="px-2 py-0.5 outline-none rounded-md "
-          />
-        </div>
-      </section>
-      <button className="bg-blue-800 hover:bg-blue-900 rounded-lg p-2 text-white">
-        Submit
-      </button>
-    </form>
-  );
+  return <FormInput />;
 };
 
 export default Form;
---
+++ src/components/FormInput.js
@@ -0,0 +1,56 @@
+import React from 'react';
+
+const FormInput = () => {
+  return (
+    <form className=" bg-cyan-600 rounded-xl p-20 flex flex-col gap-y-4">
+      <section className="flex gap-x-4">
+        <div className="">
+          <label
+            className="text-white text-lg font-semibold block mb-1"
+            htmlFor="firstname"
+          >
+            First Name
+          </label>
+          <input
+            type="text"
+            id="firstname"
+            className="px-2 py-0.5 outline-none rounded-md "
+          />
+        </div>
+        <div className="">
+          <label
+            className="text-white text-lg font-semibold block mb-1"
+            htmlFor="lastname"
+          >
+            Last Name
+          </label>
+          <input
+            type="text"
+            id="lastname"
+            className="px-2 py-0.5 outline-none rounded-md "
+          />
+        </div>
+      </section>
+      <section className="flex gap-x-4 mb-6">
+        <div className="">
+          <label
+            className="text-white text-lg font-semibold block mb-1"
+            htmlFor="email"
+          >
+            E-mail
+          </label>
+          <input
+            type="text"
+            id="email"
+            className="px-2 py-0.5 outline-none rounded-md "
+          />
+        </div>
+      </section>
+      <button className="bg-blue-800 hover:bg-blue-900 rounded-lg p-2 text-white">
+        Submit
+      </button>
+    </form>
+  );
+};
+
+export default FormInput;
